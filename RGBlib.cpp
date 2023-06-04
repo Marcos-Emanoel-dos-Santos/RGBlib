4d04f7c72078a369c621fb8d810d14d92baf605c
Eggsbennedit <113400896+Marcos-Emanoel-dos-Santos@users.noreply.github.com>
2023-06-04 15:40:46 -0300

    Update 5

--- RGBlib.cpp
+++ RGBlib.cpp
@@ -40,58 +40,51 @@ RGB::RGB(int vermelho, int azul, int verde)
 }
 void RGB::acender(String cor)
 {
-  if(cor == "Vermelho"){
-  analogWrite(_vermelho, dadosVermelho.vm);
-  analogWrite(_verde, dadosVermelho.vd);
-  analogWrite(_azul, dadosVermelho.az);
-  } else
-  if(cor == "Laranja"){
-  analogWrite(_vermelho, dadosLaranja.vm);
-  analogWrite(_verde, dadosLaranja.vd);
-  analogWrite(_azul, dadosLaranja.az);
-  } else
-  if(cor == "Amarelo"){
-  analogWrite(_vermelho, dadosAmarelo.vm);
-  analogWrite(_verde, dadosAmarelo.vd);
-  analogWrite(_azul, dadosAmarelo.az);
-  } else
-  if(cor == "Verde"){
-  analogWrite(_vermelho, dadosVerde.vm);
-  analogWrite(_verde, dadosVerde.vd);
-  analogWrite(_azul, dadosVerde.az);
-  } else
-  if(cor == "Ciano"){
-  analogWrite(_vermelho, dadosCiano.vm);
-  analogWrite(_verde, dadosCiano.vd);
-  analogWrite(_azul, dadosCiano.az);
-  } else
-  if(cor == "Azul"){
-  analogWrite(_vermelho, dadosAzul.vm);
-  analogWrite(_verde, dadosAzul.vd);
-  analogWrite(_azul, dadosAzul.az);
-  } else
-  if(cor == "Roxo"){
-  analogWrite(_vermelho, dadosRoxo.vm);
-  analogWrite(_verde, dadosRoxo.vd);
-  analogWrite(_azul, dadosRoxo.az);
-  } else
-  if(cor == "Rosa"){
-  analogWrite(_vermelho, dadosRosa.vm);
-  analogWrite(_verde, dadosRosa.vd);
-  analogWrite(_azul, dadosRosa.az);
-  } else
-  if(cor == "Marrom"){
-  analogWrite(_vermelho, dadosMarrom.vm);
-  analogWrite(_verde, dadosMarrom.vd);
-  analogWrite(_azul, dadosMarrom.az);
+  void RGB::acender(String cor) {
+  if (cor == "Vermelho") {
+    analogWrite(_vermelho, cores[0].vm);
+    analogWrite(_verde, cores[0].vd);
+    analogWrite(_azul, cores[0].az);
+  } else if (cor == "Laranja") {
+    analogWrite(_vermelho, cores[1].vm);
+    analogWrite(_verde, cores[1].vd);
+    analogWrite(_azul, cores[1].az);
+  } else if (cor == "Amarelo") {
+    analogWrite(_vermelho, cores[2].vm);
+    analogWrite(_verde, cores[2].vd);
+    analogWrite(_azul, cores[2].az);
+  } else if (cor == "Verde") {
+    analogWrite(_vermelho, cores[3].vm);
+    analogWrite(_verde, cores[3].vd);
+    analogWrite(_azul, cores[3].az);
+  } else if (cor == "Ciano") {
+    analogWrite(_vermelho, cores[4].vm);
+    analogWrite(_verde, cores[4].vd);
+    analogWrite(_azul, cores[4].az);
+  } else if (cor == "Azul") {
+    analogWrite(_vermelho, cores[5].vm);
+    analogWrite(_verde, cores[5].vd);
+    analogWrite(_azul, cores[5].az);
+  } else if (cor == "Roxo") {
+    analogWrite(_vermelho, cores[6].vm);
+    analogWrite(_verde, cores[6].vd);
+    analogWrite(_azul, cores[6].az);
+  } else if (cor == "Rosa") {
+    analogWrite(_vermelho, cores[7].vm);
+    analogWrite(_verde, cores[7].vd);
+    analogWrite(_azul, cores[7].az);
+  } else if (cor == "Marrom") {
+    analogWrite(_vermelho, cores[8].vm);
+    analogWrite(_verde, cores[8].vd);
+    analogWrite(_azul, cores[8].az);
   } else {
-  analogWrite(_vermelho, 255);
-  analogWrite(_verde, 0);
-  analogWrite(_azul, 0);
-  delay(1000);
-  analogWrite(_vermelho, 0);
-  analogWrite(_verde, 0);
-  analogWrite(_azul, 0);
-  delay(1000);
+    analogWrite(_vermelho,255);
+    analogWrite(_verde,0);
+    analogWrite(_azul,0);
+    delay(1000);
+    analogWrite(_vermelho,255);
+    analogWrite(_verde,0);
+    analogWrite(_azul,0);
+    delay(1000);
   }
 }
