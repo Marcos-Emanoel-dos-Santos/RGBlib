8334277bac4db407342f4510d678a3a9c2c9f2dd
Eggsbennedit <113400896+Marcos-Emanoel-dos-Santos@users.noreply.github.com>
2023-06-05 16:05:10 -0300

    Update 23

--- RGBlib.h
+++ RGBlib.h
@@ -20,7 +20,7 @@ class RGB
   public:
     RGB(int vermelho, int azul, int verde);
     void acender(String cor);
-    void transicao(int ms = 50);
+    void transicao(int ms = 100);
   private:
     int _vermelho; int _verde; int _azul;
     static Cor cores[];
