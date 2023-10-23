183d6c27751a593b033cacdd0f924dadcb0c18cd
Eggsbennedit <113400896+Marcos-Emanoel-dos-Santos@users.noreply.github.com>
2023-10-22 21:21:36 -0300

    Update RGBlib.cpp 2.0

--- RGBlib.cpp
+++ RGBlib.cpp
@@ -7,7 +7,6 @@ Junho de 2023
 #include "Arduino.h"
 #include "RGBlib.h"
 
-
 Cor RGB::cores[] = { // cria uma array com as informações de cada cor disponível
   {255, 0, 0},               // vermelho
   {255, 150, 0},               // laranja
@@ -45,94 +44,60 @@ RGB::RGB(int vermelho, int verde, int azul)  // define os pinos do led
 };
 
 void RGB::acender(String cor) {  // escreve a cor do led conforme informações da array cores[]
-  if (cor == "Vermelho") {
-    analogWrite(_vermelho, cores[0].vm);
-    analogWrite(_verde, cores[0].vd);
-    analogWrite(_azul, cores[0].az);
-  } else if (cor == "Laranja") {
-    analogWrite(_vermelho, cores[1].vm);
-    analogWrite(_verde, cores[1].vd);
-    analogWrite(_azul, cores[1].az);
-  } else if (cor == "Amarelo") {
-    analogWrite(_vermelho, cores[2].vm);
-    analogWrite(_verde, cores[2].vd);
-    analogWrite(_azul, cores[2].az);
-  } else if (cor == "Verde") {
-    analogWrite(_vermelho, cores[3].vm);
-    analogWrite(_verde, cores[3].vd);
-    analogWrite(_azul, cores[3].az);
-  } else if (cor == "Ciano") {
-    analogWrite(_vermelho, cores[4].vm);
-    analogWrite(_verde, cores[4].vd);
-    analogWrite(_azul, cores[4].az);
-  } else if (cor == "Azul") {
-    analogWrite(_vermelho, cores[5].vm);
-    analogWrite(_verde, cores[5].vd);
-    analogWrite(_azul, cores[5].az);
-  } else if (cor == "Roxo") {
-    analogWrite(_vermelho, cores[6].vm);
-    analogWrite(_verde, cores[6].vd);
-    analogWrite(_azul, cores[6].az);
-  } else if (cor == "Rosa") {
-    analogWrite(_vermelho, cores[7].vm);
-    analogWrite(_verde, cores[7].vd);
-    analogWrite(_azul, cores[7].az);
-  } else if (cor == "Marrom") {
-    analogWrite(_vermelho, cores[8].vm);
-    analogWrite(_verde, cores[8].vd);
-    analogWrite(_azul, cores[8].az);
-  } else if (cor == "Branco") {
-    analogWrite(_vermelho, cores[9].vm);
-    analogWrite(_verde, cores[9].vd);
-    analogWrite(_azul, cores[9].az);
-  } else {
-    analogWrite(_vermelho,255);
-    analogWrite(_verde,0);
-    analogWrite(_azul,0);
-    delay(1000);
-    analogWrite(_vermelho,255);
-    analogWrite(_verde,0);
-    analogWrite(_azul,0);
-    delay(1000);
+  for(int i=0; i<10; i++){
+    if(cor == coresRel[i]){
+      analogWrite(_vermelho, cores[i].vm);
+      analogWrite(_verde, cores[i].vd);
+      analogWrite(_azul, cores[i].az);
+    } else {
+      analogWrite(_vermelho, 255);
+      analogWrite(_verde, 0);
+      analogWrite(_azul, 0);
+      delay(1000);
+      analogWrite(_vermelho, 255);
+      analogWrite(_verde, 0);
+      analogWrite(_azul, 0);
+      delay(1000);
   }
-}
+  }
+};
 
 void RGB::rave(int ms)  // faz transição entre todas as cores do arco-íris
 {
   for(int i = 0; i < 1536; i += 32){
-    if(i < 256){ // VERMELHO -> AMARELO
-      analogWrite(_vermelho, 255);
-      analogWrite(_verde, i);
-      analogWrite(_azul, 0);
-    }
-    else if(i > 256 && i < 512){ // AMARELO -> VERDE
-      analogWrite(_vermelho, 255 - (i - 255));
-      analogWrite(_verde, 255);
-      analogWrite(_azul, 0);
-    }
-    else if(i > 512 && i < 768){ // VERDE -> CIANO
-      analogWrite(_vermelho, 0);
-      analogWrite(_verde, 255);
-      analogWrite(_azul, (i - 512));
-    }
-    else if(i > 768 && i < 1024){ // CIANO -> AZUL
-      analogWrite(_vermelho, 0);
-      analogWrite(_verde, 255 - (i-768));
-      analogWrite(_azul, 255);
-    }
-    else if(i > 1024 && i < 1280){ // AZUL -> ROSA
-      analogWrite(_vermelho, i - 1024);
-      analogWrite(_verde, 0);
-      analogWrite(_azul, 255);
-    }
-    else if(i > 1280 && i < 1536){ // AZUL -> ROSA
+    if(i >= 1280){ // AZUL -> ROSA
       analogWrite(_vermelho, 255);
       analogWrite(_verde, 0);
       analogWrite(_azul, 255 - (i - 1280));
     }
+    else if(i >= 1024){ // AZUL -> ROSA
+      analogWrite(_vermelho, i - 1024);
+      analogWrite(_verde, 0);
+      analogWrite(_azul, 255);
+    }
+    else if(i >= 768){ // CIANO -> AZUL
+      analogWrite(_vermelho, 0);
+      analogWrite(_verde, 255 - (i-768));
+      analogWrite(_azul, 255);
+    }
+    else if(i >= 512){ // VERDE -> CIANO
+      analogWrite(_vermelho, 0);
+      analogWrite(_verde, 255);
+      analogWrite(_azul, (i - 512));
+    }
+    else if(i >= 256){ // AMARELO -> VERDE
+      analogWrite(_vermelho, 255 - (i - 255));
+      analogWrite(_verde, 255);
+      analogWrite(_azul, 0);
+    }
+    else if(i <= 256){ // VERMELHO -> AMARELO
+      analogWrite(_vermelho, 255);
+      analogWrite(_verde, i);
+      analogWrite(_azul, 0);
+    }
     delay(ms);
   }
-}
+};
 
 void RGB::transicao(String cor1, String cor2, int ms)  // faz transição entre duas cores específicas
 {
@@ -154,9 +119,9 @@ void RGB::transicao(String cor1, String cor2, int ms)  // faz transição entre 
       _cor2 = coresRel[i].indice;
     }
   }
-  vmDiff = cores[_cor1].vm - cores[_cor2].vm;
-  vdDiff = cores[_cor1].vd - cores[_cor2].vd;
-  azDiff = cores[_cor1].az - cores[_cor2].az;
+  double vmDiff = cores[_cor1].vm - cores[_cor2].vm;
+  double vdDiff = cores[_cor1].vd - cores[_cor2].vd;
+  double azDiff = cores[_cor1].az - cores[_cor2].az;
   for(int i=0; i<100; i++){
     analogWrite(_vermelho, vmDiff/(ms/100));
     analogWrite(_verde, vdDiff/(ms/100));
